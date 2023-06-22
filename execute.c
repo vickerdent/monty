24e16424fcf2fc525422673148d22f51c8ad110d
Promise12345678 <promisesmutenhaunga@gmail.com>
2023-06-22 14:48:41 -0700

    Attack 1

--- execute.c
+++ execute.c
@@ -1,40 +1,52 @@
 #include "monty.h"
 /**
- * f_div - serves to divide the top two elements of the stack.
- * @head: serves as stack head
- * @counter: serves as line_number
- * Return: no return
+* execute - serves to execute the opcode
+* @stack: head linked list - stack
+* @counter: line_counter
+* @file: poiner to monty file
+* @content: line content
+* Return: no return
 */
-void f_div(stack_t **head, unsigned int counter)
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
-    stack_t *h;
-    int len = 0, aux;
+	instruction_t opst[] = {
+				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+				{"pop", f_pop},
+				{"swap", f_swap},
+				{"add", f_add},
+				{"nop", f_nop},
+				{"sub", f_sub},
+				{"div", f_div},
+				{"mul", f_mul},
+				{"mod", f_mod},
+				{"pchar", f_pchar},
+				{"pstr", f_pstr},
+				{"rotl", f_rotl},
+				{"rotr", f_rotr},
+				{"queue", f_queue},
+				{"stack", f_stack},
+				{NULL, NULL}
+				};
+	unsigned int i = 0;
+	char *op;
 
-    h = *head;
-    while (h)
-    {
-        h = h->next;
-        len++;
-    }
-    if (len < 2)
-    {
-        fprintf(stderr, "L%d: can't div, stack too short\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-    h = *head;
-    if (h->n == 0)
-    {
-        fprintf(stderr, "L%d: division by zero\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-    aux = h->next->n / h->n;
-    h->next->n = aux;
-    *head = h->next;
-    free(h);
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[i].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); }
+	return (1);
 }
